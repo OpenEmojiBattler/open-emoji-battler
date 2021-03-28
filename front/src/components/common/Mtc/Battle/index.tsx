2238c58a1e2dc6a8b5baf4855017264c4e239b1f
tash-2s <81064017+tash-2s@users.noreply.github.com>
2021-03-28 15:14:23 +0900

    front: Fix damage amount bug on Battle
    It must include player grades.

--- front/src/components/common/Mtc/Battle/index.tsx
+++ front/src/components/common/Mtc/Battle/index.tsx
@@ -28,10 +28,10 @@ export function Battle(props: { mtcState: MtcState; finish: () => void }) {
 
   const onBoardsFinish = (playerBoardGrade: number, rivalBoardGrade: number) => {
     if (playerBoardGrade !== 0) {
-      setRivalHealthDamage(playerBoardGrade)
+      setRivalHealthDamage(playerBoardGrade + props.mtcState.grade)
     }
     if (rivalBoardGrade !== 0) {
-      setPlayerHealthDamage(rivalBoardGrade)
+      setPlayerHealthDamage(rivalBoardGrade + gradeAndBoardGhost.grade.toNumber())
     }
   }
 
