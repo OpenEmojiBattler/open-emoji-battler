5a24dfecec6ce98b6dfc95c0c4de6db717c761c9
tash-2s <81064017+tash-2s@users.noreply.github.com>
2021-04-14 13:12:05 +0900

    front: Remove style check view

--- front/src/components/pages/Style.tsx
+++ front/src/components/pages/Style.tsx
@@ -13,44 +13,7 @@ export function Style() {
   return (
     <section className="section">
       <div className={"container"}>
-        <div className={"block"}>
-          <div className={"emo-group"}>
-            {[10, 20, 30].map((l) => {
-              return [20, 30, 40].map((s) => {
-                return (
-                  <div key={`${l}:${s}`} className={"emo-group-line"}>
-                    <span>{s}:{l}</span>
-                    {[0, 72, 144, 216, 288].map((h) => {
-                      return (
-                        <div key={h} className="emo">
-                          <div className="emo-body-outer">
-                            <div
-                              className="emo-body-inner"
-                              style={{
-                                backgroundImage: `radial-gradient(circle closest-side, hsl(${h},${s}%,${
-                                  l + 10
-                                }%), hsl(${h},${s}%,${l}%))`,
-                              }}
-                            >
-                              <div className="emo-body-inner-emoji">💸</div>
-                              <div className="emo-body-inner-grade">𝟝</div>
-                              <div className="emo-body-inner-specials">
-                                <span className="emo-body-inner-specials-shield">▣</span>
-                                <span className="emo-body-inner-specials-attractive">✪</span>
-                              </div>
-                              <div className="emo-body-inner-attack">60</div>
-                              <div className="emo-body-inner-health">20</div>
-                            </div>
-                          </div>
-                        </div>
-                      )
-                    })}
-                  </div>
-                )
-              })
-            })}
-          </div>
-        </div>
+        <div className={"block"}></div>
       </div>
     </section>
   )
