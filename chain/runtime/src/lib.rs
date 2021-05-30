902e60c94ec57d410ff69846228cc5cd36b480d9
tash-2s <81064017+tash-2s@users.noreply.github.com>
2021-05-30 09:49:47 +0900

    chain: Bump spec version

--- chain/runtime/src/lib.rs
+++ chain/runtime/src/lib.rs
@@ -94,7 +94,7 @@ pub const VERSION: RuntimeVersion = RuntimeVersion {
     spec_name: create_runtime_str!("open-emoji-battler"),
     impl_name: create_runtime_str!("open-emoji-battler"),
     authoring_version: 1,
-    spec_version: 9,
+    spec_version: 10,
     impl_version: 1,
     apis: RUNTIME_API_VERSIONS,
     transaction_version: 1,
