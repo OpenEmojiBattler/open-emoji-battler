9b8a5aa970b73a54251787dfd877577b726cdbf0
tash-2s <81064017+tash-2s@users.noreply.github.com>
2021-05-31 01:02:30 +0900

    script: exec get playdrop data

--- onetime-scripts/20210519_getFirstPlayAirdropClaimedAddresses.ts
+++ onetime-scripts/20210519_getFirstPlayAirdropClaimedAddresses.ts
@@ -3,15 +3,14 @@ import { encodeAddress } from "@polkadot/util-crypto"
 
 import { connected, getEnv, query } from "common"
 
-connected(getEnv("local").endpoint, async () =>
+connected(getEnv("production").endpoint, async () =>
   query((q) => q.firstAirdrop.playerAirdropDestinationKusamaAccountId.entries()).then((entries) => {
     const data = {} as any
     entries.forEach(([key, value]) => {
       const chainAddress = key.args[0].toString()
       const substrateAddress = value.unwrap().toString()
       const kusamaAddress = encodeAddress(substrateAddress, 2)
-      // console.log(substrateAddress, kusamaAddress)
-      data[chainAddress] = kusamaAddress
+      data[chainAddress] = [substrateAddress, kusamaAddress]
     })
 
     writeFileSync(
