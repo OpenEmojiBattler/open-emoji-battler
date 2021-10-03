954ed96487e051f4a8cbfd995bf1e4bfade16486
tash-2s <81064017+tash-2s@users.noreply.github.com>
2021-10-03 22:23:43 +0900

    contracts/logic: Impl finish_mtc_shop

--- contracts/logic/lib.rs
+++ contracts/logic/lib.rs
@@ -7,8 +7,14 @@ pub mod contract {
     use common::codec_types::*;
     #[cfg(not(feature = "ink-as-dependency"))]
     use common::mtc::{
-        emo_bases::check_and_build_emo_bases, setup::build_pool, shop::coin::get_upgrade_coin,
-        utils::PLAYER_INITIAL_HEALTH,
+        battle::organizer::{battle_all, select_battle_ghost_index},
+        emo_bases::check_and_build_emo_bases,
+        setup::build_pool,
+        shop::{
+            coin::{decrease_upgrade_coin, get_upgrade_coin},
+            player_operation::verify_player_operations_and_update,
+        },
+        utils::{get_turn_and_previous_grade_and_board, PLAYER_INITIAL_HEALTH},
     };
     #[cfg(not(feature = "ink-as-dependency"))]
     use ink_env::call::FromAccountId;
@@ -95,8 +101,85 @@ pub mod contract {
         }
 
         #[ink(message)]
-        pub fn finish_mtc_shop(&self, player_operations: StdVec<mtc::shop::PlayerOperation>) {
-            // FIXME
+        pub fn finish_mtc_shop(
+            &self,
+            caller: AccountId,
+            player_operations: StdVec<mtc::shop::PlayerOperation>,
+        ) {
+            let mut storage = Storage::from_account_id(self.storage_account_id);
+
+            let emo_bases = storage.get_emo_bases();
+            let grade_and_board_history = storage
+                .get_player_grade_and_board_history(caller)
+                .expect("player_grade_and_board_history none");
+            let mut upgrade_coin = storage.get_player_upgrade_coin(caller);
+
+            let (
+                turn,
+                mtc::GradeAndBoard {
+                    mut grade,
+                    mut board,
+                },
+            ) = get_turn_and_previous_grade_and_board(&grade_and_board_history);
+
+            board = verify_player_operations_and_update(
+                board,
+                &mut grade,
+                &mut upgrade_coin,
+                &player_operations,
+                &storage.get_player_pool(caller).expect("player_pool none"),
+                storage.get_player_seed(caller).expect("player_seed none"),
+                turn,
+                &emo_bases,
+            )
+            .expect("invalid shop player operations");
+
+            let mut health = storage
+                .get_player_health(caller)
+                .expect("player_health none");
+            let battle_ghost_index = storage
+                .get_player_battle_ghost_index(caller)
+                .expect("battle_ghost_index none");
+            let mut ghost_states = storage
+                .get_player_ghost_states(caller)
+                .expect("ghost_states none");
+
+            let new_seed = self.get_random_seed(caller, b"finish_mtc_shop");
+
+            let ghosts = storage
+                .get_player_ghosts(caller)
+                .expect("player_ghosts none")
+                .into_iter()
+                .map(|(_, ghost)| ghost)
+                .collect::<StdVec<_>>(); // FIXME: EP
+
+            let final_place = battle_all(
+                &board,
+                &mut health,
+                &mut ghost_states,
+                grade,
+                &ghosts,
+                battle_ghost_index,
+                turn,
+                new_seed,
+                &emo_bases,
+            )
+            .expect("battle failed");
+
+            self.finish(
+                &mut storage,
+                caller,
+                grade,
+                board,
+                new_seed,
+                upgrade_coin,
+                battle_ghost_index,
+                health,
+                ghost_states,
+                // &ghost_eps,
+                grade_and_board_history,
+                final_place,
+            );
         }
 
         fn cleanup_finished(&self, storage: &mut Storage, account: AccountId) {
@@ -129,6 +212,94 @@ pub mod contract {
 
         fn matchmake(&self, account_id: AccountId, storage: &mut Storage, seed: u64) {
             // TODO
+        }
+
+        fn finish(
+            &self,
+            storage: &mut Storage,
+            account_id: AccountId,
+            grade: u8,
+            board: mtc::Board,
+            new_seed: u64,
+            upgrade_coin: Option<u8>,
+            battle_ghost_index: u8,
+            health: u8,
+            ghost_states: StdVec<mtc::GhostState>,
+            // ghost_eps: &[u16],
+            mut grade_and_board_history: StdVec<mtc::GradeAndBoard>,
+            final_place: Option<u8>,
+        ) {
+            grade_and_board_history.push(mtc::GradeAndBoard { grade, board });
+
+            if let Some(place) = final_place {
+                self.finish_mtc(
+                    storage,
+                    account_id,
+                    place,
+                    &ghost_states,
+                    // ghost_eps,
+                    &grade_and_board_history,
+                );
+            } else {
+                self.finish_battle(
+                    storage,
+                    account_id,
+                    upgrade_coin,
+                    ghost_states,
+                    battle_ghost_index,
+                    new_seed,
+                    health,
+                    grade_and_board_history,
+                );
+            }
+
+            storage.set_player_seed(account_id, new_seed);
+        }
+
+        fn finish_mtc(
+            &self,
+            storage: &mut Storage,
+            account_id: AccountId,
+            place: u8,
+            ghost_states: &[mtc::GhostState],
+            // ghost_eps: &[u16],
+            grade_and_board_history: &[mtc::GradeAndBoard],
+        ) {
+            // let ep = Self::_update_ep(account_id, place, ghost_states, ghost_eps)?;
+
+            if place < 4 {
+                // Self::_register_ghost(account_id, ep, grade_and_board_history);
+            }
+
+            self.cleanup_finished(storage, account_id);
+        }
+
+        fn finish_battle(
+            &self,
+            storage: &mut Storage,
+            account_id: AccountId,
+            upgrade_coin: Option<u8>,
+            ghost_states: StdVec<mtc::GhostState>,
+            battle_ghost_index: u8,
+            new_seed: u64,
+            health: u8,
+            grade_and_board_history: StdVec<mtc::GradeAndBoard>,
+        ) {
+            if grade_and_board_history.len() > 30 {
+                panic!("max turn exceeded");
+            }
+
+            let upgrade_coin = decrease_upgrade_coin(upgrade_coin);
+
+            let new_battle_ghost_index =
+                select_battle_ghost_index(&ghost_states, battle_ghost_index, new_seed)
+                    .expect("battle ghost selection failed");
+
+            storage.set_player_grade_and_board_history(account_id, grade_and_board_history);
+            storage.set_player_health(account_id, health);
+            storage.set_player_ghost_states(account_id, ghost_states);
+            storage.set_player_battle_ghost_index(account_id, new_battle_ghost_index);
+            self.update_upgrade_coin(storage, account_id, upgrade_coin);
         }
 
         #[ink(message)]
