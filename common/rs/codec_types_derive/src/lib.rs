f3a28d35d04e9e00c313432935305a87dc1917de
tash-2s <81064017+tash-2s@users.noreply.github.com>
2022-01-16 10:39:44 +0900

    rs: Fix for contract storage layout

--- common/rs/codec_types_derive/src/lib.rs
+++ common/rs/codec_types_derive/src/lib.rs
@@ -3,9 +3,8 @@ extern crate proc_macro;
 use proc_macro::TokenStream;
 use quote::quote;
 
-// We want to use this type for only PackedLayout.
-// PackedLayout requires SpreadLayout, so leave it empty.
-#[proc_macro_derive(EmptySpreadLayout)]
+// layout for packed, one-cell storage items, to support recursive types with boxes
+#[proc_macro_derive(SpreadLayoutOneStorageCell)]
 pub fn codec_types_empty_spread_layout_derive(input: TokenStream) -> TokenStream {
     let ast = syn::parse(input).unwrap();
     impl_empty_spread_layout_macro(&ast)
@@ -17,26 +16,19 @@ fn impl_empty_spread_layout_macro(ast: &syn::DeriveInput) -> TokenStream {
     let gen = quote! {
         impl ink_storage::traits::SpreadLayout for #name {
             const FOOTPRINT: u64 = 1;
+            const REQUIRES_DEEP_CLEAN_UP: bool = false;
 
-            fn pull_spread(_ptr: &mut ink_storage::traits::KeyPtr) -> Self {
-                unimplemented!(
-                    "EmptySpreadLayout: unimplemented pull_spread: {}",
-                    stringify!(#name)
-                );
+
+            fn pull_spread(ptr: &mut ink_storage::traits::KeyPtr) -> Self {
+                ink_storage::traits::forward_pull_packed::<Self>(ptr)
             }
 
-            fn push_spread(&self, _ptr: &mut ink_storage::traits::KeyPtr) {
-                unimplemented!(
-                    "EmptySpreadLayout: unimplemented push_spread: {}",
-                    stringify!(#name)
-                );
+            fn push_spread(&self, ptr: &mut ink_storage::traits::KeyPtr) {
+                ink_storage::traits::forward_push_packed::<Self>(self, ptr)
             }
 
-            fn clear_spread(&self, _ptr: &mut ink_storage::traits::KeyPtr) {
-                unimplemented!(
-                    "EmptySpreadLayout: unimplemented clear_spread: {}",
-                    stringify!(#name)
-                );
+            fn clear_spread(&self, ptr: &mut ink_storage::traits::KeyPtr) {
+                ink_storage::traits::forward_clear_packed::<Self>(self, ptr)
             }
         }
     };
