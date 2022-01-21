OpenEmojiBattler/open-emoji-battler

commit 1a6e115f3ed7968b2d3fa50632b2f66c66526fb6

Misc for contracts
https://github.com/paritytech/ink/pull/942

common/rs/src/mtc/emo_bases.rs
@@ -1,0 +1,36 @@
+use crate::codec_types::*;
+use sp_std::prelude::*;
+
+pub fn check_and_build_emo_bases(
+    mut bases: emo::Bases,
+    new_bases: emo::Bases,
+    fixed_base_ids: &[u16],
+    built_base_ids: &[u16],
+    force_bases_update: bool,
+) -> Result<emo::Bases, &'static str> {
+    if force_bases_update {
+        bases = new_bases;
+    } else {
+        for (id, value) in new_bases.0.into_iter() {
+            if bases.0.contains_key(&id) {
+                continue;
+            }
+            bases.0.insert(id, value);
+        }
+    }
+
+    let base_keys = bases.0.keys().cloned().collect::<Vec<_>>();
+
+    for id in fixed_base_ids.iter() {
+        if base_keys.contains(id) {
+            return Err("invalid emo bases: fixed_base_ids");
+        }
+    }
+    for id in built_base_ids.iter() {
+        if base_keys.contains(id) {
+            return Err("invalid emo bases: built_base_ids");
+        }
+    }
+
+    Ok(bases)
+}

chain/pallets/game/src/lib.rs
@@ -4,6 +4,7 @@ use common::{
     codec_types::*,
     mtc::{
         battle::organizer::{battle_all, select_battle_ghost_index},
+        emo_bases::check_and_build_emo_bases,
         ep::{calculate_new_ep, get_ep_band, EP_UNFINISH_PENALTY, INITIAL_EP},
         result::build_ghost_from_history,
         setup::{build_initial_ghost_states, build_pool},
@@ -16,7 +17,7 @@ use common::{
     utils::partial_bytes_to_u64,
 };
 use frame_support::{
-    debug::native::debug, dispatch::DispatchResultWithPostInfo, ensure, traits::Randomness,
+    debug::native::debug, dispatch::DispatchResultWithPostInfo, traits::Randomness,
 };
 use parity_scale_codec::Encode;
 use rand::seq::SliceRandom;
@@ -141,12 +142,20 @@ pub mod pallet {
         ) -> DispatchResultWithPostInfo {
             ensure_root(origin)?;
 
-            Self::_update_emo_bases(
+            let bases = check_and_build_emo_bases(
+                <EmoBases<T>>::get().unwrap_or_else(emo::Bases::new),
                 new_bases,
-                fixed_base_ids,
-                built_base_ids,
+                &fixed_base_ids,
+                &built_base_ids,
                 force_bases_update,
             )
+            .map_err(|_e| Error::<T>::InvalidEmoBases)?;
+
+            <EmoBases<T>>::put(bases);
+            <DeckFixedEmoBaseIds<T>>::put(fixed_base_ids);
+            <DeckBuiltEmoBaseIds<T>>::put(built_base_ids);
+
+            Ok(().into())
         }
 
         #[pallet::weight(1)]
@@ -190,41 +199,6 @@ pub mod pallet {
 }
 
 impl<T: Config> Pallet<T> {
-    fn _update_emo_bases(
-        new_bases: emo_Bases,
-        fixed_base_ids: Vec<u16>,
-        built_base_ids: Vec<u16>,
-        force_bases_update: bool,
-    ) -> DispatchResultWithPostInfo {
-        let mut bases = <EmoBases<T>>::get().unwrap_or_else(emo::Bases::new);
-
-        if force_bases_update {
-            bases = new_bases;
-        } else {
-            for (id, value) in new_bases.0.into_iter() {
-                if bases.0.contains_key(&id) {
-                    continue;
-                }
-                bases.0.insert(id, value);
-            }
-        }
-
-        let base_keys = bases.0.keys().cloned().collect::<Vec<_>>();
-
-        for id in fixed_base_ids.iter() {
-            ensure!(base_keys.contains(id), Error::<T>::InvalidEmoBases);
-        }
-        for id in built_base_ids.iter() {
-            ensure!(base_keys.contains(id), Error::<T>::InvalidEmoBases);
-        }
-
-        <EmoBases<T>>::put(bases);
-        <DeckFixedEmoBaseIds<T>>::put(fixed_base_ids);
-        <DeckBuiltEmoBaseIds<T>>::put(built_base_ids);
-
-        Ok(().into())
-    }
-
     fn _start_mtc(
         main: T::AccountId,
         session: T::AccountId,

common/rs/Cargo.toml
@@ -14,7 +14,8 @@ blake2-rfc = { version = "0.2.18", default-features = false }
 libm = { version = "0.2.1", default-features = false }
 
 scale-info = { version = "0.6", default-features = false, features = ["derive"], optional = true }
-ink_storage = { version = "3.0.0-rc5", default-features = false, optional = true }
+ink_storage = { git = "https://github.com/paritytech/ink", rev = "fd94d38", default-features = false, optional = true }
+ink_primitives = { git = "https://github.com/paritytech/ink", rev = "fd94d38", default-features = false, optional = true }
 
 [dev-dependencies]
 hex = { version = "0.4.2" }
@@ -29,8 +30,8 @@ std = [
     "anyhow/std",
 ]
 view-logs = []
-ink = ["scale-info", "ink_storage"]
-ink-std = ["ink", "scale-info/std", "ink_storage/std"]
+ink = ["scale-info", "ink_storage", "ink_primitives"]
+ink-std = ["std", "ink", "scale-info/std", "ink_storage/std", "ink_primitives/std"]
 
 [[bench]]
 name = "my_benchmark"

common/rs/src/mtc.rs
@@ -1,5 +1,6 @@
 pub mod battle;
 pub mod decoders;
+pub mod emo_bases;
 pub mod ep;
 pub mod result;
 pub mod setup;

contracts/forwarder/Cargo.toml
@@ -7,12 +7,12 @@ license = "Apache-2.0"
 authors = ["Open Emoji Battler"]
 
 [dependencies]
-ink_primitives = { version = "3.0.0-rc5", default-features = false }
-ink_metadata = { version = "3.0.0-rc5", default-features = false, features = ["derive"], optional = true }
-ink_env = { version = "3.0.0-rc5", default-features = false }
-ink_storage = { version = "3.0.0-rc5", default-features = false }
-ink_prelude = { version = "3.0.0-rc5", default-features = false }
-ink_lang = { version = "3.0.0-rc5", default-features = false }
+ink_primitives = { git = "https://github.com/paritytech/ink", rev = "fd94d38", default-features = false }
+ink_metadata = { git = "https://github.com/paritytech/ink", rev = "fd94d38", default-features = false, features = ["derive"], optional = true }
+ink_env = { git = "https://github.com/paritytech/ink", rev = "fd94d38", default-features = false }
+ink_storage = { git = "https://github.com/paritytech/ink", rev = "fd94d38", default-features = false }
+ink_prelude = { git = "https://github.com/paritytech/ink", rev = "fd94d38", default-features = false }
+ink_lang = { git = "https://github.com/paritytech/ink", rev = "fd94d38", default-features = false }
 
 scale = { package = "parity-scale-codec", version = "2.1", default-features = false, features = ["derive"] }
 scale-info = { version = "0.6.0", default-features = false, features = ["derive"], optional = true }

contracts/logic/Cargo.toml
@@ -7,19 +7,19 @@ license = "Apache-2.0"
 authors = ["Open Emoji Battler"]
 
 [dependencies]
-ink_primitives = { version = "3.0.0-rc5", default-features = false }
-ink_metadata = { version = "3.0.0-rc5", default-features = false, features = ["derive"], optional = true }
-ink_env = { version = "3.0.0-rc5", default-features = false }
-ink_storage = { version = "3.0.0-rc5", default-features = false }
-ink_prelude = { version = "3.0.0-rc5", default-features = false }
-ink_lang = { version = "3.0.0-rc5", default-features = false }
+ink_primitives = { git = "https://github.com/paritytech/ink", rev = "fd94d38", default-features = false }
+ink_metadata = { git = "https://github.com/paritytech/ink", rev = "fd94d38", default-features = false, features = ["derive"], optional = true }
+ink_env = { git = "https://github.com/paritytech/ink", rev = "fd94d38", default-features = false }
+ink_storage = { git = "https://github.com/paritytech/ink", rev = "fd94d38", default-features = false }
+ink_prelude = { git = "https://github.com/paritytech/ink", rev = "fd94d38", default-features = false }
+ink_lang = { git = "https://github.com/paritytech/ink", rev = "fd94d38", default-features = false }
 
 scale = { package = "parity-scale-codec", version = "2.1", default-features = false, features = ["derive"] }
 scale-info = { version = "0.6.0", default-features = false, features = ["derive"], optional = true }
 
 storage = { version = "0.1.0", path = "../storage", default-features = false, features = ["ink-as-dependency"] }
 
-common = { path = '../../common/rs', version = '0.1.0', default-features = false }
+common = { path = "../../common/rs", version = "0.1.0", default-features = false, features = ["ink"] }
 
 [lib]
 name = "logic"
@@ -43,6 +43,6 @@ std = [
 
     "storage/std",
 
-    'common/std',
+    "common/ink-std",
 ]
 ink-as-dependency = []

contracts/logic/lib.rs
@@ -4,7 +4,7 @@ use ink_lang as ink;
 
 #[ink::contract]
 pub mod contract {
-    use common::codec_types::*;
+    use common::{codec_types::*, mtc::emo_bases::check_and_build_emo_bases};
     use ink_env::call::FromAccountId;
     use ink_prelude::vec::Vec as StdVec;
     use storage::contract::Storage;
@@ -12,23 +12,21 @@ pub mod contract {
     #[ink(storage)]
     pub struct Logic {
         storage_account_id: AccountId,
+        admin_account_id: AccountId,
     }
 
     impl Logic {
         #[ink(constructor)]
-        pub fn new(storage_account_id: AccountId) -> Self {
-            Self { storage_account_id }
+        pub fn new(storage_account_id: AccountId, admin_account_id: AccountId) -> Self {
+            Self {
+                storage_account_id,
+                admin_account_id,
+            }
         }
 
         #[ink(message)]
         pub fn get_storage_account_id(&self) -> AccountId {
             self.storage_account_id
-        }
-
-        #[ink(message)]
-        pub fn do_something(&self) {
-            let mut storage = Storage::from_account_id(self.storage_account_id);
-            storage.set(true);
         }
 
         #[ink(message)]
@@ -39,7 +37,63 @@ pub mod contract {
             built_base_ids: StdVec<u16>,
             force_bases_update: bool,
         ) {
+            self.only_admin_caller();
+
+            let mut storage = Storage::from_account_id(self.storage_account_id);
+
+            let bases = check_and_build_emo_bases(
+                storage.get_emo_bases(),
+                new_bases,
+                &fixed_base_ids,
+                &built_base_ids,
+                force_bases_update,
+            )
+            .unwrap();
+
+            storage.set_emo_bases(bases);
+            storage.set_deck_fixed_emo_base_ids(fixed_base_ids);
+            storage.set_deck_built_emo_base_ids(built_base_ids);
+        }
+
+        #[ink(message)]
+        pub fn start_mtc(&self, deck_emo_base_ids: [u16; 6]) {
+            let caller = self.env().caller();
+            let mut storage = Storage::from_account_id(self.storage_account_id);
+
+            if storage.get_player_pool(caller).is_some() {
+                self.cleanup_finished(&mut storage, caller);
+            }
             // FIXME
+
+            let seed = get_random_seed();
+        }
+
+        #[ink(message)]
+        pub fn finish_mtc_shop(&self, player_operations: StdVec<mtc::shop::PlayerOperation>) {
+            // FIXME
+        }
+
+        fn cleanup_finished(&self, storage: &mut Storage, account: AccountId) {
+            storage.take_player_pool(account);
+            storage.take_player_health(account);
+            storage.take_player_grade_and_board_history(account);
+            storage.take_player_upgrade_coin(account);
+            storage.take_player_ghosts(account);
+            storage.take_player_ghost_states(account);
+            storage.take_player_battle_ghost_index(account);
+        }
+
+        #[ink(message)]
+        pub fn get_admin_account_id(&self) -> AccountId {
+            self.admin_account_id.clone()
+        }
+
+        fn only_admin_caller(&self) {
+            assert!(
+                self.admin_account_id == self.env().caller(),
+                "only_admin_caller: caller is not admin ({:?})",
+                &self.env().caller()
+            );
         }
     }
 }

contracts/storage/Cargo.toml
@@ -7,15 +7,17 @@ license = "Apache-2.0"
 authors = ["Open Emoji Battler"]
 
 [dependencies]
-ink_primitives = { version = "3.0.0-rc5", default-features = false }
-ink_metadata = { version = "3.0.0-rc5", default-features = false, features = ["derive"], optional = true }
-ink_env = { version = "3.0.0-rc5", default-features = false }
-ink_storage = { version = "3.0.0-rc5", default-features = false }
-ink_prelude = { version = "3.0.0-rc5", default-features = false }
-ink_lang = { version = "3.0.0-rc5", default-features = false }
+ink_primitives = { git = "https://github.com/paritytech/ink", rev = "fd94d38", default-features = false }
+ink_metadata = { git = "https://github.com/paritytech/ink", rev = "fd94d38", default-features = false, features = ["derive"], optional = true }
+ink_env = { git = "https://github.com/paritytech/ink", rev = "fd94d38", default-features = false }
+ink_storage = { git = "https://github.com/paritytech/ink", rev = "fd94d38", default-features = false }
+ink_prelude = { git = "https://github.com/paritytech/ink", rev = "fd94d38", default-features = false }
+ink_lang = { git = "https://github.com/paritytech/ink", rev = "fd94d38", default-features = false }
 
 scale = { package = "parity-scale-codec", version = "2.1", default-features = false, features = ["derive"] }
 scale-info = { version = "0.6.0", default-features = false, features = ["derive"], optional = true }
+
+common = { path = "../../common/rs", version = "0.1.0", default-features = false, features = ["ink"] }
 
 [lib]
 name = "storage"
@@ -36,5 +38,7 @@ std = [
 
     "scale/std",
     "scale-info/std",
+
+    "common/ink-std",
 ]
 ink-as-dependency = []

commit 99f3c41b8338e2f7a3a81cd8c08a0ffa03edb815

Fix bug introduced in https://github.com/OpenEmojiBattler/open-emoji-battler/commit/1a6e115f3ed7968b2d3fa50632b2f66c66526fb6

common/rs/src/mtc/emo_bases.rs
@@ -22,12 +22,12 @@ pub fn check_and_build_emo_bases(
     let base_keys = bases.0.keys().cloned().collect::<Vec<_>>();
 
     for id in fixed_base_ids.iter() {
-        if base_keys.contains(id) {
+        if !base_keys.contains(id) {
             return Err("invalid emo bases: fixed_base_ids");
         }
     }
     for id in built_base_ids.iter() {
-        if base_keys.contains(id) {
+        if !base_keys.contains(id) {
             return Err("invalid emo bases: built_base_ids");
         }
     }