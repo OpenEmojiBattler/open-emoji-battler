fae62448f4c702b4d9937c4eba8a5d4950891365
tash-2s <81064017+tash-2s@users.noreply.github.com>
2022-02-06 13:13:20 +0900

    chain: init.sh is not used anymore

--- chain/scripts/init.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-
-set -e
-
-echo "*** Initializing WASM build environment"
-
-rustup default nightly-2021-03-01
-
-rustup target add wasm32-unknown-unknown --toolchain nightly-2021-03-01
