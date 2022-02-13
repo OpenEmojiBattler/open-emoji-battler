b10f9a3e6922e5d51319728516dfb864eacbefaf
tash-2s <81064017+tash-2s@users.noreply.github.com>
2022-02-13 18:00:03 +0900

    js: Check env types

--- common/js/src/env.ts
+++ common/js/src/env.ts
@@ -2,9 +2,10 @@ import localEnv from "./envs/local.json"
 import stagingEnv from "./envs/staging.json"
 import productionEnv from "./envs/production.json"
 
-export type EnvContract = typeof productionEnv["contract"]
+type Env = typeof productionEnv
+export type EnvContract = Env["contract"]
 
-export const getEnv = (envName: any) => {
+export const getEnv = (envName: any): Env => {
   if (!envName) {
     throw new Error(`no envName: ${envName}`)
   }
