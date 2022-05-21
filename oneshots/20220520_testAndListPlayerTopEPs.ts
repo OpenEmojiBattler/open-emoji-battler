f5ca711613b2490f419ae424afe4289bc7ab2f30
tash-2s <81064017+tash-2s@users.noreply.github.com>
2022-05-21 21:55:12 +0900

    oneshots: Limit top eps

--- oneshots/20220520_testAndListPlayerTopEPs.ts
+++ oneshots/20220520_testAndListPlayerTopEPs.ts
@@ -10,21 +10,25 @@ const data: Data = JSON.parse(readFileSync(dataFilePath, "utf8"))
 const dump = data.bestEP
 
 connected(getEnv("production").chainEndpoint, async (api) => {
-  const current = (await api.query.game.playerEp.entries()).reduce<Record<string, number>>(
-    (obj, [key, value]) => {
-      obj[key.args[0].toString()] = value.unwrap().toNumber()
-      return obj
-    },
-    {}
+  // const current = (await api.query.game.playerEp.entries()).reduce<Record<string, number>>(
+  //   (obj, [key, value]) => {
+  //     obj[key.args[0].toString()] = value.unwrap().toNumber()
+  //     return obj
+  //   },
+  //   {}
+  // )
+
+  // for (const [addr, ep] of Object.entries(dump)) {
+  //   if (current[addr] > ep) {
+  //     throw new Error(`data diff: ${addr}`)
+  //   }
+  // }
+
+  const sortedDump = Object.fromEntries(
+    Object.entries(dump)
+      .filter(([, e]) => e >= 1025)
+      .sort(([, a], [, b]) => a - b)
   )
-
-  for (const [addr, ep] of Object.entries(dump)) {
-    if (current[addr] > ep) {
-      throw new Error(`data diff: ${addr}`)
-    }
-  }
-
-  const sortedDump = Object.fromEntries(Object.entries(dump).sort(([, a], [, b]) => a - b))
 
   writeFileSync(
     "./20220520_testAndListPlayerTopEPs.json",
