afff737a7bd7a6e023059f30123af70930c65c6d
tash-2s <81064017+tash-2s@users.noreply.github.com>
2022-06-02 22:35:16 +0900

    Revert "front: Adapt new react dom api"
    This reverts commit 228118c6c91b68a320793ffb5435b9f2cebbedde.
    
    We're not ready for multiple mount/unmount.
    https://reactjs.org/blog/2022/03/08/react-18-upgrade-guide.html#updates-to-strict-mode

--- front/src/index.ts
+++ front/src/index.ts
@@ -1,13 +1,11 @@
 import { createElement } from "react"
-import { createRoot } from "react-dom/client"
+import { render } from "react-dom"
 
 import "./index.sass"
 
 import { App } from "./components/App"
 import { setEmojiFavicon } from "./misc/favicon"
 
-const con = document.getElementById("app-js-connector")
-if (!con) throw new Error("Failed to find con")
-createRoot(con).render(createElement(App))
+render(createElement(App), document.getElementById("app-js-connector"))
 
 setEmojiFavicon("👑")
