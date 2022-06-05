a7d081b9c1f36676cfd705675195813bd621e03e
tash-2s <81064017+tash-2s@users.noreply.github.com>
2022-06-05 15:00:26 +0900

    oneshots: Map target addresses and emoji

--- oneshots/20220605_initNFT.ts
+++ oneshots/20220605_initNFT.ts
@@ -6,16 +6,22 @@ const main = async () => {
   const gameplay1: Array<string> = Object.values<[string, string]>(
     JSON.parse(readFileSync("./20210519_getFirstPlayAirdropClaimedAddresses.json", "utf8"))
   ).map(([_s, k]) => k)
+  const twitter = readAddresses("./20210613_twitterGiveawayWinners.json")
   const rmrk = readAddresses("./20210611_rmrkValidClaimedAddresses.json")
   const unique = readAddresses("./20210611_uniqueValidClaimedAddresses.json")
-  const twitter = readAddresses("./20210613_twitterGiveawayWinners.json")
   const gameplay2 = readAddresses("./20220524_mergeTargetPlayers.json")
 
-  testAddresses(gameplay1)
-  testAddresses(rmrk)
-  testAddresses(unique)
-  testAddresses(twitter)
-  testAddresses(gameplay2)
+  const target: Array<[string, Array<string>]> = [
+    ["🍭", gameplay1],
+    ["🤯", rmrk],
+    ["📦", unique],
+    ["🦕", twitter.concat(gameplay2)],
+  ]
+
+  target.forEach(([e, a]) => {
+    console.log(e)
+    testAddresses(a)
+  })
 }
 
 const readAddresses = (path: string): Array<string> =>
