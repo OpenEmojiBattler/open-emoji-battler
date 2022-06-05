f4a2439c2738cf93111d8ed34a50ea67176dbb64
tash-2s <81064017+tash-2s@users.noreply.github.com>
2022-06-05 18:49:25 +0900

    oneshots: Divide steps

--- oneshots/20220605_initNFT.ts
+++ oneshots/20220605_initNFT.ts
@@ -2,8 +2,9 @@ import { readFileSync } from "fs"
 
 import { encodeAddress } from "@polkadot/util-crypto"
 import { stringToHex } from "@polkadot/util"
+import type { SubmittableExtrinsic } from "@polkadot/api/types"
 
-import { connected } from "common"
+import { connected, range } from "common"
 import { getKeyringPair } from "common/src/scriptUtils"
 
 const main = async () => {
@@ -15,19 +16,35 @@ const main = async () => {
   )
 
   const sender = await getKeyringPair(process.argv[2])
+
+  const step = Number(process.argv[3])
+  if (![0, 1, 2, 3, 4].includes(step)) {
+    throw new Error("invalid step")
+  }
+
   console.log(`sender: ${sender.address}`)
 
   await connected(
     "wss://westmint-rpc.polkadot.io",
     async (api) => {
-      const txs = [
-        api.tx.uniques.create(collectionId, sender.address),
-        api.tx.uniques.setClassMetadata(collectionId, collectionMetadata, false),
-      ]
+      const txs: Array<SubmittableExtrinsic<"promise">> = []
 
-      let itemId = 0
+      if (step === 0) {
+        txs.push(
+          api.tx.uniques.create(collectionId, sender.address),
+          api.tx.uniques.setClassMetadata(collectionId, collectionMetadata, false)
+        )
+      } else {
+        const targetIndex = step - 1
 
-      for (const [emoji, addresses] of target) {
+        let itemId = 0
+        if (targetIndex !== 0) {
+          itemId = range(targetIndex)
+            .map((n) => target[n][1].length)
+            .reduce((a, b) => a + b)
+        }
+
+        const [emoji, addresses] = target[targetIndex]
         const itemMetadata = buildMetadata(
           `<svg xmlns='http://www.w3.org/2000/svg' viewBox='0 -7 9 9'><text font-size='7'>${emoji}</text></svg>`
         )
