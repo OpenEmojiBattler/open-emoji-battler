05dd8f7a6b613c4864f8ff14b508cd805b43cea0
tash-2s <81064017+tash-2s@users.noreply.github.com>
2022-08-21 18:11:43 +0900

    oneshots: Update analysis script

--- oneshots/20220821_analyzeGhostLastBoards.ts
+++ oneshots/20220821_analyzeGhostLastBoards.ts
@@ -1,17 +1,24 @@
 import data from "./20220821_getGhostLastBoards.json"
 
+const countStrings = (strArr: string[]) =>
+  Object.fromEntries(
+    strArr
+      .reduce<[string, number][]>((arr, cur) => {
+        const f = arr.find((o) => o[0] === cur)
+        if (f) {
+          f[1] += 1
+        } else {
+          arr.push([cur, 1])
+        }
+        return arr
+      }, [])
+      .sort(([_a, a], [_b, b]) => b - a)
+  )
+
 for (const [epBand, lastBoards] of Object.entries(data)) {
   console.log(epBand)
-  console.log(
-    lastBoards
-      .flatMap(({ board }) => board.map((e) => e.typ))
-      .reduce<Record<string, number>>((obj, cur) => {
-        if (obj[cur]) {
-          obj[cur] += 1
-        } else {
-          obj[cur] = 1
-        }
-        return obj
-      }, {})
-  )
+
+  console.log(countStrings(lastBoards.flatMap(({ board }) => board.map((e) => e.typ))))
+
+  console.log(lastBoards.map(({ board }) => countStrings(board.map((e) => e.typ))))
 }
