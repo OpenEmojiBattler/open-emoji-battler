8aa3a1d080885fb5ec68cff61966e25bb8bf717a
tash-2s <81064017+tash-2s@users.noreply.github.com>
2022-09-02 22:39:24 +0900

    contract: Use new script stracture: oneshots/connectionTemplate.ts

--- contract/deploy/202109210_init/script.ts
+++ contract/deploy/202109210_init/script.ts
@@ -1,47 +1,45 @@
 import { readFileSync } from "fs"
 import { resolve } from "path"
 
-import { txContract, connected } from "common"
+import { txContract, connect } from "common"
 import { loadEmoBases } from "common/src/scriptUtils"
 import { instantiateContract, getEndpointAndPair } from "../utils"
 
 const main = async () => {
   const { envName, endpoint, keyringPair } = await getEndpointAndPair()
 
-  await connected(
-    endpoint,
-    async (api) => {
-      const gameContract = await instantiateContract(
-        api,
-        keyringPair,
-        "game",
-        [],
-        __dirname,
-        envName,
-        "../../game/target/ink/game.contract" // remove this later
-      )
+  const api = await connect(endpoint, false)
+  const gameContract = await instantiateContract(
+    api,
+    keyringPair,
+    "game",
+    [],
+    __dirname,
+    envName,
+    "../../game/target/ink/game.contract" // remove this later
+  )
 
-      const availableEmoBaseIds = JSON.parse(
-        readFileFromFileRelativePath("./availableEmoBaseIds.json")
-      )
+  const availableEmoBaseIds = JSON.parse(readFileFromFileRelativePath("./availableEmoBaseIds.json"))
 
-      await txContract(
-        gameContract,
-        "updateEmoBases",
-        [
-          loadEmoBases(readFileFromFileRelativePath("./emoBases.json")).toU8a(),
-          availableEmoBaseIds.fixed,
-          availableEmoBaseIds.built,
-          true,
-        ],
-        keyringPair
-      )
-    },
-    false
+  await txContract(
+    gameContract,
+    "updateEmoBases",
+    [
+      loadEmoBases(readFileFromFileRelativePath("./emoBases.json")).toU8a(),
+      availableEmoBaseIds.fixed,
+      availableEmoBaseIds.built,
+      true,
+    ],
+    keyringPair
   )
 }
 
 const readFileFromFileRelativePath = (path: string) =>
   readFileSync(resolve(__dirname, path), "utf8")
 
-main().catch(console.error).finally(process.exit)
+main()
+  .then(() => process.exit())
+  .catch((e) => {
+    console.error(e)
+    process.exit(1)
+  })
