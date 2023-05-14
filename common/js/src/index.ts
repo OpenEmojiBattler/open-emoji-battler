3bab3448b2b5777415b5a767ab8d3c04f419021e
tash-2s <81064017+tash-2s@users.noreply.github.com>
2023-05-14 16:12:54 -0500

    js: Don't use `.js` file extensions for now
    This code is not ready for that.
    
    ref https://github.com/polkadot-js/api/issues/5524

--- common/js/src/index.ts
+++ common/js/src/index.ts
@@ -1,6 +1,6 @@
-import "./interfaces/augment-api.js"
-import "./interfaces/augment-types.js"
-import "./interfaces/types-lookup.js"
+import "./interfaces/augment-api"
+import "./interfaces/augment-types"
+import "./interfaces/types-lookup"
 
 export * from "./api"
 export * from "./pow"
